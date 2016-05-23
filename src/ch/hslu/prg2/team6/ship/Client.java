18eccf5b38618e4f870325660c1c27f808fd90ba
Tim Egeli <play337@gmail.com>
2016-05-23 10:01:21 +0200

    Created client and server class

--- src/ch/hslu/prg2/team6/ship/Client.java
+++ src/ch/hslu/prg2/team6/ship/Client.java
@@ -9,11 +9,16 @@ import java.util.HashSet;
  * Created by Tim Egeli on 22/05/2016.
  */
 public class Client {
+    private int id;
+
+    public Client(int id) {
+        this.id = id;
+    }
 
     public void sendShotField(String field) {
         try (DatagramSocket socket = new DatagramSocket()){
             InetAddress address = InetAddress.getByName("localhost");
-            byte[] message = field.getBytes();
+            byte[] message = (field+id).getBytes();
             DatagramPacket packet = new DatagramPacket(message, message.length, address, 42321);
             socket.send(packet);
             socket.receive(packet);
--- src/ch/hslu/prg2/team6/ship/Game.java
+++ src/ch/hslu/prg2/team6/ship/Game.java
@@ -4,8 +4,44 @@ package ch.hslu.prg2.team6.ship;
  * Created by Tim Egeli on 10/05/2016.
  */
 public class Game {
+    private boolean isServer = true;
+
     public void main(String[] args) {
-        ShipModel s = new ShipModel("A1", "C2");
-        System.out.println("Hello");
+        //ShipModel s = new ShipModel("A1", "C2");
+        startGame();
+    }
+    private void startGame(){
+        Server server = new Server(); // Singleton?!
+        //ShipView gui = new ShipView();
+        //gui.displayGameType(); // or just start the game via main menu?
+    }
+
+    public void startServer(){
+        Server server = new Server();
+        //while (!server.isReady()){
+        // sleep()
+        //}
+        createPlayer(1);
+
+
+        // avoid multiple instances?
+
+    }
+
+    public void startClient(){
+        //Client client = new Client();
+
+        //while (!client.isReady()){
+        // sleep()
+        //}
+        createPlayer(2);
+
+        // avoid multiple instances?
+
+    }
+    private void createPlayer(int id){
+        Client client = new Client(id);
+        //BattlefieldModel fieldModel = new BattlefieldModel(id);
+        //gui.displayModel(fieldModel);
     }
 }
