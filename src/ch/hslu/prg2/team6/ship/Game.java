22fb086fb4a7842bfda2663108feb84dd0cd5d30
Tim Egeli <play337@gmail.com>
2016-05-24 09:25:31 +0200

    Updated something

--- src/ch/hslu/prg2/team6/ship/Game.java
+++ src/ch/hslu/prg2/team6/ship/Game.java
@@ -8,8 +8,8 @@ public class Game {
 
     public static void main(String[] args) {
         Game g = new Game();
-        //g.startServer();
-        g.startClient();
+        g.startServer();
+        //g.startClient();
     }
 
     private void startGame(){
